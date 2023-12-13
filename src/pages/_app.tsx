20f547f871cf5af953a5f20dd6d92b963103c4cb
jamestran1 <quang.tran@episerver.com>
2023-12-13 13:55:13 +0700

    Add Apollo authentication and OptiOidcProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,30 +2,64 @@ import '@/styles/globals.css'
 import { Layout } from '@/components/Layout/Layout';
 
 import type { AppProps } from 'next/app';
-import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink } from '@apollo/client';
+import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, concat, createHttpLink } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { SessionProvider, useSession } from "next-auth/react"
 import { ExtendedSession } from '@/components/LoginBtn';
 
 const httpLink = createHttpLink({
-  uri: 'https://cg.optimizely.com/content/v2?auth=NsfqFlN2UN1srJEEJyBGobakhagSIbmpSZBhyseRJHuaqoJ2&tenant_id=299a9bb409ef4b0ba10f15e824d8f1f4',
+  uri: 'https://cg.optimizely.com/content/v2?auth=NsfqFlN2UN1srJEEJyBGobakhagSIbmpSZBhyseRJHuaqoJ2',
 });
 
-function getPreviewToken(): string{
+function getPreviewToken(): string {
   const queryString = window?.location?.search;
-  if(!queryString) {
-      return ""
+  if (!queryString) {
+    return ""
   }
 
   const urlParams = new URLSearchParams(queryString);
   return urlParams.get('preview_token')?.toString() ?? ""
 }
 
-let authLink: ApolloLink;
+// let authLink: ApolloLink;
 
-let client = new ApolloClient({
-  link: httpLink,
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const preview_token = getPreviewToken();
+  const token = localStorage.getItem("accessToken")
+  console.log("token", token)
+  if (token) {
+    return {
+      uri: 'https://cg.optimizely.com/content/v2',
+      headers: {
+        ...headers,
+        preview_token: preview_token,
+        authorization: `Bearer ${token}`,
+        "CG-Tenant-Id": "299a9bb409ef4b0ba10f15e824d8f1f4"
+      }
+    }
+  }
+  return {
+    headers: {
+      ...headers,
+      preview_token: preview_token,
+    }
+  }
+});
+
+function Auth({ children }: any) {
+  const { data: session } = useSession({ required: false })
+  const extendedSession: ExtendedSession = session as ExtendedSession
+  if (extendedSession?.token?.accessToken) {
+    localStorage.setItem("accessToken", extendedSession.token.accessToken)
+  }
+
+  return children
+}
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -41,33 +75,6 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
       </Auth>
     </SessionProvider>
   );
-}
-
-function Auth({ children }:any) {
-  const { data: session } = useSession({required: false})
-  const extendedSession: ExtendedSession = session as ExtendedSession
-  if(extendedSession?.token?.access_token) {
-
-    authLink = setContext((_, { headers }) => {
-      // get the authentication token from local storage if it exists
-      const preview_token = getPreviewToken();
-      // return the headers to the context so httpLink can read them
-      return {
-        headers: {
-          ...headers,
-          preview_token: preview_token,
-          Authorization: 'Bearer ' + extendedSession.token.access_token
-        }
-      }
-    });
-    
-    client = new ApolloClient({
-      link: authLink.concat(httpLink),
-      cache: new InMemoryCache(),
-    });
-  }
-
-  return children
 }
 
 export default MyApp;
